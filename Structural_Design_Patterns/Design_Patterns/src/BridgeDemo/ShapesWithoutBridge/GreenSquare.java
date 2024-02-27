532f005a1137d48603e62a9d29f09220829d2360
Tae Hui Chang <jasmine878@yahoo.com>
2024-02-26 22:05:00 -0600

    Add a GreenSquare without using a Bridge Pattern

---
+++ Structural_Design_Patterns/Design_Patterns/src/BridgeDemo/ShapesWithoutBridge/GreenSquare.java
@@ -0,0 +1,8 @@
+package BridgeDemo.ShapesWithoutBridge;
+
+public class GreenSquare extends Square {
+    @Override
+    public void applyColor() {
+        System.out.println("Applying green color");
+    }
+}
--- Structural_Design_Patterns/Design_Patterns/src/BridgeDemo/ShapesWithoutBridge/ShapesWithoutBridgeDemo.java
+++ Structural_Design_Patterns/Design_Patterns/src/BridgeDemo/ShapesWithoutBridge/ShapesWithoutBridgeDemo.java
@@ -3,10 +3,11 @@ package BridgeDemo.ShapesWithoutBridge;
 public class ShapesWithoutBridgeDemo {
     public static void main(String[] args) {
         Circle circle = new BlueCircle();
-
         Square square = new RedSquare();
+        Square square2 = new GreenSquare();
 
         circle.applyColor();
         square.applyColor();
+        square2.applyColor();
     }
 }
