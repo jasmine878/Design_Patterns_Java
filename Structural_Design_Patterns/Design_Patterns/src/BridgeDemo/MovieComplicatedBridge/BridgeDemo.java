6412641cd91c63957b825597db9a5658c4f7883b
Tae Hui Chang <jasmine878@yahoo.com>
2024-03-03 16:04:47 -0600

    Create an Html Formatter

--- Structural_Design_Patterns/Design_Patterns/src/BridgeDemo/MovieComplicatedBridge/BridgeDemo.java
+++ Structural_Design_Patterns/Design_Patterns/src/BridgeDemo/MovieComplicatedBridge/BridgeDemo.java
@@ -2,16 +2,25 @@ package BridgeDemo.MovieComplicatedBridge;
 
 public class BridgeDemo {
     public static void main(String[] args) {
+        //Create movie object
         Movie movie = new Movie();
         movie.setClassification("Action");
         movie.setTitle("John Wick");
         movie.setYear("2014");
         movie.setRuntime("2:15");
 
+        //Pass movie object to Print Formatter
         Formatter printFormatter = new PrintFormatter();
         Printer moviePrinter = new MoviePrinter(movie);
 
         String printedMaterial = moviePrinter.print(printFormatter);
         System.out.println(printedMaterial);
+
+        //Pass movie object to Html Formatter
+        Formatter htmlFormatter = new HtmlFormatter();
+        String htmlMaterial = moviePrinter.print(htmlFormatter);
+
+        System.out.println(htmlMaterial);
+
     }
 }
