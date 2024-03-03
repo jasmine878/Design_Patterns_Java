05e2b94eab7b2eed65bd1619a80f51e30d2c6bfd
Tae Hui Chang <jasmine878@yahoo.com>
2024-03-03 01:14:31 -0600

    Finalize Bridge with a Print Formatter

--- Structural_Design_Patterns/Design_Patterns/src/BridgeDemo/MovieComplicatedBridge/BridgeDemo.java
+++ Structural_Design_Patterns/Design_Patterns/src/BridgeDemo/MovieComplicatedBridge/BridgeDemo.java
@@ -7,5 +7,11 @@ public class BridgeDemo {
         movie.setTitle("John Wick");
         movie.setYear("2014");
         movie.setRuntime("2:15");
+
+        Formatter printFormatter = new PrintFormatter();
+        Printer moviePrinter = new MoviePrinter(movie);
+
+        String printedMaterial = moviePrinter.print(printFormatter);
+        System.out.println(printedMaterial);
     }
 }
