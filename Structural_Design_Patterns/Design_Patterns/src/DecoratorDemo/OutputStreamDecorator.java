644c65bc0c78474a5edaf2c0dfb7090baa33dd01
Tae Hui Chang <jasmine878@yahoo.com>
2024-03-14 22:52:15 -0500

    OutputStream Decorator example

---
+++ Structural_Design_Patterns/Design_Patterns/src/DecoratorDemo/OutputStreamDecorator.java
@@ -0,0 +1,14 @@
+package DecoratorDemo;
+
+import java.io.*;
+
+public class OutputStreamDecorator {
+    public static void main(String[] args) throws IOException {
+        File file = new File("./output.txt");
+        file.createNewFile();
+        OutputStream oStream = new FileOutputStream(file);
+
+        DataOutputStream doStream = new DataOutputStream(oStream);
+        doStream.writeChars("text");
+    }
+}
