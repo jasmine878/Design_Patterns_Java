255c1b2fa6625e0a6b81f7bf90bc50cd1a411b88
Tae Hui Chang <jasmine878@yahoo.com>
2024-03-19 22:30:24 -0500

    Create a URL Facade Pattern

---
+++ Structural_Design_Patterns/Design_Patterns/src/Facade/FacadeURLDemo.java
@@ -0,0 +1,19 @@
+package Facade;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URI;
+import java.net.URL;
+
+public class FacadeURLDemo {
+    public static void main(String[] args) throws Exception {
+        URL url = new URI("https://app.pluralsight.com/profile/author/bryan-hansen").toURL();
+        BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
+
+        String inputLine;
+
+        while((inputLine = in.readLine()) != null) {
+            System.out.println(inputLine);
+        }
+    }
+}
