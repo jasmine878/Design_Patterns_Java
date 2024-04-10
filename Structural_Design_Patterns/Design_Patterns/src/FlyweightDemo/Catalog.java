fcb4241bba6a6ae41362c6732de94b7b7a9f58f6
Tae Hui Chang <jasmine878@yahoo.com>
2024-04-09 22:42:53 -0500

    Create models

---
+++ Structural_Design_Patterns/Design_Patterns/src/FlyweightDemo/Catalog.java
@@ -0,0 +1,22 @@
+package FlyweightDemo;
+
+import java.util.HashMap;
+import java.util.Map;
+
+//Catalog acts as a Factory and cache for Item flyweight objects
+public class Catalog {
+    private Map<String, Item> items = new HashMap<>();
+
+    //factory method
+    public Item lookup(String itemName) {
+        if (!items.containsKey(itemName)) {
+            items.put(itemName, new Item(itemName));
+        }
+
+        return items.get(itemName);
+    }
+
+    public int totalItemsMade() {
+        return items.size();
+    }
+}
---
+++ Structural_Design_Patterns/Design_Patterns/src/FlyweightDemo/Item.java
@@ -0,0 +1,13 @@
+package FlyweightDemo;
+
+public class Item {
+    private final String name;
+
+    public Item(String name) {
+        this.name = name;
+    }
+
+    public String toString() {
+        return name;
+    }
+}
